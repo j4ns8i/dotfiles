52f6516af9ab0ec11951034f62052adc8f26c0dc
j4ns8i <jsmalkowski+github@gmail.com>
2024-02-02 22:58:35 -0500

    Neovim: add telescope-fzf-native plugin and pf highlighting mapping

--- roles/neovim/files/config/nvim/after/plugin/telescope.lua
+++ roles/neovim/files/config/nvim/after/plugin/telescope.lua
@@ -1,6 +1,20 @@
+require('telescope').setup({
+  extensions = {
+    fzf = {
+      fuzzy = true,
+      override_generic_sorter = true,
+      override_file_sorter = true,
+      case_mode = "smart_case",
+    }
+  }
+})
+
+require('telescope').load_extension('fzf')
+
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+vim.keymap.set('n', '<leader>ph', builtin.highlights, {})
 vim.keymap.set('n', '<C-p>', builtin.git_files, {})
 vim.keymap.set('n', '<leader>ps', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+  builtin.grep_string({ search = vim.fn.input("Find in files: ") })
 end)
--- roles/neovim/files/config/nvim/lua/justin/packer.lua
+++ roles/neovim/files/config/nvim/lua/justin/packer.lua
@@ -8,8 +8,10 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
-    -- or                            , branch = '0.1.x',
-    requires = { { 'nvim-lua/plenary.nvim' } }
+    requires = {
+      { 'nvim-lua/plenary.nvim' },
+      { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' },
+    }
   }
 
   use({
