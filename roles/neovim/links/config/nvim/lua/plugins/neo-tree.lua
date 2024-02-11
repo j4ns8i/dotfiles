23898ab5a34fa73e9745d06d39fd0d0f694d5595
j4ns8i <jsmalkowski+github@gmail.com>
2024-02-10 22:55:23 -0500

    Neovim: Add basic neo-tree mappings

--- roles/neovim/links/config/nvim/lua/plugins/neo-tree.lua
+++ roles/neovim/links/config/nvim/lua/plugins/neo-tree.lua
@@ -5,6 +5,16 @@ return {
       'nvim-lua/plenary.nvim',
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
+    },
+    lazy = false,
+    keys = {
+      { '<leader>ntf', '<cmd>Neotree<cr>' },
+      { '<leader>ntt', '<cmd>Neotree toggle<cr>' },
+    },
+    opts = {
+      filesystem = {
+        hijack_netrw_behavior = 'open_current',
+      },
     }
   },
 }
