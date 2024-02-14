cbb91a9e4844520280d92a794b20231d9e5c2348
j4ns8i <jsmalkowski+github@gmail.com>
2024-02-13 22:32:44 -0500

    Neovim: Lazy load neo-tree

--- roles/neovim/links/config/nvim/lua/plugins/neo-tree.lua
+++ roles/neovim/links/config/nvim/lua/plugins/neo-tree.lua
@@ -6,18 +6,32 @@ return {
       'nvim-tree/nvim-web-devicons',
       'MunifTanjim/nui.nvim',
     },
-    lazy = false, -- because it needs to be loaded in order to check if we're opening a directory
     keys = {
       { '<leader>ntf', '<cmd>Neotree<cr>' },
       { '<leader>ntt', '<cmd>Neotree toggle<cr>' },
     },
+    cmd = 'Neotree',
+    init = function()
+      vim.api.nvim_create_autocmd('BufEnter', {
+        group = vim.api.nvim_create_augroup('NeoTreeInit', { clear = true }),
+        callback = function()
+          local f = vim.fn.expand('%:p')
+          if vim.fn.isdirectory(f) ~= 0 then
+            vim.cmd('Neotree left dir=' .. f)
+            vim.o.number = false
+            vim.o.relativenumber = false
+            vim.api.nvim_clear_autocmds { group = 'NeoTreeInit' }
+          end
+        end
+      })
+    end,
     opts = {
       filesystem = {
         hijack_netrw_behavior = 'open_default',
         window = {
           mappings = {
             ["gs"] = function()
-              vim.cmd [[ Neotree float git_status ]]
+              vim.cmd('Neotree float git_status')
             end,
           },
         },
