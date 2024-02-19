896205fedb46b0a553d5cb8e61daea759d999566
j4ns8i <jsmalkowski+github@gmail.com>
2024-02-19 11:08:23 -0500

    Neovim: Add none-ls with shellcheck

---
+++ roles/neovim/links/config/nvim/lua/plugins/none-ls.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "nvimtools/none-ls.nvim",
+    opts = function()
+      local null_ls = require("null-ls")
+      return {
+        sources = {
+          null_ls.builtins.diagnostics.shellcheck,
+        },
+      }
+    end,
+  },
+}
