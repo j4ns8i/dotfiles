0250c8bbfffd0ecf233e027b32d67f01bd4244fb
j4ns8i <jsmalkowski+github@gmail.com>
2024-02-28 17:01:02 -0500

    Neovim: Replace telescope live_grep with live_grep_args
    Mappings:
    - <c-k>: quote current search term (to provide directory as second arg, e.g.)
    - <c-f>: quote current search term and query specific file types
    - <c-i>: quote current search term and ignore a glob

--- roles/neovim/links/config/nvim/lua/plugins/telescope.lua
+++ roles/neovim/links/config/nvim/lua/plugins/telescope.lua
@@ -3,7 +3,8 @@ return {
   version = '0.1.5',
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    { 'nvim-telescope/telescope-fzf-native.nvim',     build = 'make', },
+    { 'nvim-telescope/telescope-live-grep-args.nvim', version = '^1.0.0', },
     { 'nvim-tree/nvim-web-devicons' },
     { 'folke/trouble.nvim' },
   },
@@ -11,18 +12,29 @@ return {
     { '<leader>pf', function() require('telescope.builtin').find_files() end },
     { '<leader>ph', function() require('telescope.builtin').highlights() end },
     { '<leader>pg', function() require('telescope.builtin').git_files() end },
-    { '<leader>ps', function() require('telescope.builtin').live_grep() end },
+    { '<leader>ps', function() require('telescope').extensions.live_grep_args.live_grep_args() end },
   },
-  opts = function()
+  config = function()
     local trouble = require('trouble.providers.telescope')
-    return {
+    local lga = require('telescope-live-grep-args.actions')
+    local opts = {
       extensions = {
         fzf = {
           fuzzy = true,
           override_generic_sorter = true,
           override_file_sorter = true,
           case_mode = "smart_case",
-        }
+        },
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              ['<c-k>'] = lga.quote_prompt(),
+              ['<c-i>'] = lga.quote_prompt({ postfix = ' --iglob ' }),
+              ['<c-f>'] = lga.quote_prompt({ postfix = ' -t ' }),
+            },
+          },
+        },
       },
       defaults = {
         mappings = {
@@ -37,11 +49,11 @@ return {
         layout_config = { height = 0.95, preview_height = 0.60 },
       },
     }
-  end,
-  config = function(_, opts)
+
     local plugin = require('telescope')
     plugin.setup(opts)
     plugin.load_extension('fzf')
+    plugin.load_extension('live_grep_args')
 
     local c = require('justin.colors')
     c.hi('TelescopeBorder', { ctermfg = c.darkgray })
