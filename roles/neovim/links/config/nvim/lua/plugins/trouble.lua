536cb8559b224c6120d4645c098b87a1fc073ef1
j4ns8i <jsmalkowski+github@gmail.com>
2024-03-11 22:18:29 -0400

    Neovim: HACK: wait 50ms after incoming_calls() before opening quickfix with Trouble

--- roles/neovim/links/config/nvim/lua/plugins/trouble.lua
+++ roles/neovim/links/config/nvim/lua/plugins/trouble.lua
@@ -4,12 +4,19 @@ return {
     'nvim-tree/nvim-web-devicons'
   },
   keys = {
-    {'<leader>xx', function () require('trouble').close() end },
-    {'<leader>xw', function () require('trouble').open('workspace_diagnostics') end },
-    {'<leader>xd', function () require('trouble').open('document_diagnostics') end },
-    {'<leader>xq', function () require('trouble').open('quickfix') end },
-    {'<leader>xl', function () require('trouble').open('loclist') end },
-    {'gR',         function () require('trouble').open('lsp_references') end },
-    {'<leader>xi', function () require('trouble').open('lsp_implementations') end },
+    { '<leader>xx', function() require('trouble').close() end },
+    { '<leader>xw', function() require('trouble').open('workspace_diagnostics') end },
+    { '<leader>xc', function()
+      vim.lsp.buf.incoming_calls()
+      -- hack: wait for the previous call to finish because it's async and can't be awaited
+      vim.cmd.sleep('50m')
+      vim.cmd.cclose()
+      require('trouble').open('quickfix')
+    end },
+    { '<leader>xd', function() require('trouble').open('document_diagnostics') end },
+    { '<leader>xq', function() require('trouble').open('quickfix') end },
+    { '<leader>xl', function() require('trouble').open('loclist') end },
+    { 'gR',         function() require('trouble').open('lsp_references') end },
+    { '<leader>xi', function() require('trouble').open('lsp_implementations') end },
   },
 }
