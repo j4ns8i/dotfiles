4766f0d6e7f78aca0270721005434399be10e6af
j4ns8i <jsmalkowski+github@gmail.com>
2024-03-30 12:13:01 -0400

    style(ansible): apply a first pass of ansible-lint recommendations

--- .commitlintrc.mjs
+++ .commitlintrc.mjs
@@ -5,7 +5,7 @@ const roles = await readdir('./roles')
 const Configuration = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['docs', 'feat', 'fix', 'ops', 'refactor', 'revert']],
+    'type-enum': [2, 'always', ['docs', 'feat', 'fix', 'ops', 'refactor', 'revert', 'style']],
     'scope-enum': [2, 'always', ['ansible', 'deps', ...roles]],
   },
 };
