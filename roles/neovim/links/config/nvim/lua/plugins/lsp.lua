fa6de019a31b5a299cc4600391d5b05abe37ca0e
j4ns8i <jsmalkowski+github@gmail.com>
2024-07-16 14:22:15 -0400

    refactor(neovim): reduce repetition for lsp configuration

--- roles/neovim/links/config/nvim/lua/plugins/lsp.lua
+++ roles/neovim/links/config/nvim/lua/plugins/lsp.lua
@@ -1,179 +1,125 @@
-return {
-  {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'justin',
-    },
-    lazy = false,
-    keys = {
-      { '<leader>wl', vim.cmd.LspInfo, desc = 'Open LspInfo window' },
-    },
-    config = function()
-      local c = require('justin.colors')
-      c.hi('LspInfoBorder', { link = 'FloatBorder' })
-      vim.lsp.handlers['textDocument/hover'] =
-          vim.lsp.with(
-            vim.lsp.handlers.hover,
-            {
-              border = 'rounded',
-            }
-          )
+-- setup an lsp with default and optional custom configuration. lsp_opts is
+-- a table of elements including an lsp name and the optional config.
+local function setup_lsp(lsp_opts)
+  local lspconfig = require('lspconfig')
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-      vim.lsp.handlers['textDocument/signatureHelp'] =
-          vim.lsp.with(
-            vim.lsp.handlers.signature_help,
-            {
-              border = 'rounded',
-            }
-          )
+  -- Use an on_attach function to only map the following keys
+  -- after the language server attaches to the current buffer
+  local on_attach = function(_, bufnr)
+    -- Enable completion triggered by <c-x><c-o>
+    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-      require('lspconfig.ui.windows').default_options.border = 'rounded'
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, bufopts)
+    vim.keymap.set('n', '<leader>K', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+  end
 
-      local opts = { noremap = true, silent = true }
-      vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
-      vim.keymap.set('n', '<M-k>', vim.diagnostic.goto_prev, opts)
-      vim.keymap.set('n', '<M-j>', vim.diagnostic.goto_next, opts)
-      vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+  local lsp_name = lsp_opts[1]
+  local extra_opts = lsp_opts[2]
 
-      -- Set "<" prefix before diagnostic virtual text
-      vim.diagnostic.config({
-        virtual_text = { prefix = '<' },
-        float = { border = 'rounded' },
-        signs = false, -- disable signs in sign column
-      })
+  local opts = {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
 
-      -- Use an on_attach function to only map the following keys
-      -- after the language server attaches to the current buffer
-      local on_attach = function(_, bufnr)
-        -- Enable completion triggered by <c-x><c-o>
-        vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  if extra_opts ~= nil then
+    for k, v in pairs(extra_opts) do
+      opts[k] = v
+    end
+  end
 
-        -- Mappings.
-        -- See `:help vim.lsp.*` for documentation on any of the below functions
-        local bufopts = { noremap = true, silent = true, buffer = bufnr }
-        vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-        vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-        vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, bufopts)
-        vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, bufopts)
-        vim.keymap.set('n', '<leader>K', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
-        vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-        vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-      end
+  lspconfig[lsp_name].setup(opts)
+end
 
-      local lspconfig = require('lspconfig')
-      local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+-- config is the lazy config function
+local function config()
+  local c = require('justin.colors')
+  c.hi('LspInfoBorder', { link = 'FloatBorder' })
+  vim.lsp.handlers['textDocument/hover'] =
+      vim.lsp.with(
+        vim.lsp.handlers.hover,
+        {
+          border = 'rounded',
+        }
+      )
 
-      lspconfig['gopls'].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-        settings = {
-          gopls = {
-            buildFlags = { '-tags', 'functional,integration' },
-            semanticTokens = true,
-            usePlaceholders = true,
-          }
+  vim.lsp.handlers['textDocument/signatureHelp'] =
+      vim.lsp.with(
+        vim.lsp.handlers.signature_help,
+        {
+          border = 'rounded',
+        }
+      )
+
+  require('lspconfig.ui.windows').default_options.border = 'rounded'
+
+  local opts = { noremap = true, silent = true }
+  vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '<M-k>', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '<M-j>', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
+
+  -- Set "<" prefix before diagnostic virtual text
+  vim.diagnostic.config({
+    virtual_text = { prefix = '<' },
+    float = { border = 'rounded' },
+    signs = false,     -- disable signs in sign column
+  })
+
+  local lspconfig = require('lspconfig')
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+  local lsps = {
+    {
+      'gopls',
+      settings = {
+        gopls = {
+          buildFlags = { '-tags', 'functional,integration' },
+          semanticTokens = true,
+          usePlaceholders = true,
         }
       }
-
-      lspconfig['dockerls'].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      }
-
-      lspconfig['docker_compose_language_service'].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      }
-
-      lspconfig['rust_analyzer'].setup {
-        on_attach = on_attach,
-        capabilities = capabilities
-      }
-
-      lspconfig['pyright'].setup {
-        on_attach = on_attach,
-        capabilities = capabilities,
-      }
-
-      lspconfig['volar'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['terraformls'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['tflint'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['bashls'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['jsonnet_ls'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['tsserver'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['cssls'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['emmet_language_server'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['tailwindcss'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      -- This goes haywire on current react project
-      lspconfig['eslint'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['jsonls'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['html'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['templ'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['ansiblels'].setup({
-        on_attach = on_attach,
-        capabilities = capabilities,
-      })
-
-      lspconfig['lua_ls'].setup({
-        on_attach = on_attach,
-        on_init = (function(client)
+    },
+    { 'dockerls' },
+    {
+      'starlark_rust',
+      { filetypes = { 'starlark' } }
+    },
+    { 'docker_compose_language_service' },
+    { 'rust_analyzer' },
+    { 'zls' },
+    { 'pyright' },
+    { 'volar' },
+    { 'terraformls' },
+    { 'tflint' },
+    { 'bashls' },
+    { 'jsonnet_ls' },
+    { 'tsserver' },
+    { 'cssls' },
+    { 'emmet_language_server' },
+    { 'pyright' },
+    { 'tailwindcss' },
+    { 'eslint' },
+    { 'jsonls' },
+    { 'html' },
+    { 'templ' },
+    { 'ansiblels' },
+    {
+      'lua_ls',
+      {
+        on_init = function(client)
           local path = client.workspace_folders[1].name
           if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
             client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
@@ -200,9 +146,27 @@ return {
             client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
           end
           return true
-        end),
-        capabilities = capabilities,
-      })
-    end
+        end,
+      }
+    }
+  }
+
+  for _, v in pairs(lsps) do
+    setup_lsp(v)
+  end
+end
+
+return {
+  {
+    'neovim/nvim-lspconfig',
+    dependencies = {
+      'hrsh7th/cmp-nvim-lsp',
+      'justin',
+    },
+    lazy = false,
+    keys = {
+      { '<leader>wl', vim.cmd.LspInfo, desc = 'Open LspInfo window' },
+    },
+    config = config
   },
 }
