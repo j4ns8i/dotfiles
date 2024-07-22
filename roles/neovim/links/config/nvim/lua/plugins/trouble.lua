e42850e0e781d5a2d8e1cdc92be13ed0bbed2712
j4ns8i <jsmalkowski+github@gmail.com>
2024-07-22 14:18:36 -0400

    feat(neovim): better trouble incoming calls and symbols

--- roles/neovim/links/config/nvim/lua/plugins/trouble.lua
+++ roles/neovim/links/config/nvim/lua/plugins/trouble.lua
@@ -6,17 +6,12 @@ return {
   keys = {
     { '<leader>xx', function() require('trouble').close() end },
     { '<leader>xw', function() require('trouble').open('workspace_diagnostics') end },
-    { '<leader>xc', function()
-      vim.lsp.buf.incoming_calls()
-      -- hack: wait for the previous call to finish because it's async and can't be awaited
-      vim.cmd.sleep('50m')
-      vim.cmd.cclose()
-      require('trouble').open('quickfix')
-    end },
+    { '<leader>xc', function() require('trouble').open('lsp_incoming_calls') end },
     { '<leader>xd', function() require('trouble').open('document_diagnostics') end },
     { '<leader>xq', function() require('trouble').open('quickfix') end },
     { '<leader>xl', function() require('trouble').open('loclist') end },
     { 'gR',         function() require('trouble').open('lsp_references') end },
     { '<leader>xi', function() require('trouble').open('lsp_implementations') end },
+    { '<leader>xs', function() require('trouble').open('lsp_document_symbols') end },
   },
 }
