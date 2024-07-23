591e075ad74f3c7a3f7c9442731de0f8dc9e839e
j4ns8i <jsmalkowski+github@gmail.com>
2024-07-23 16:55:23 -0400

    fix(neovim): lua_ls setup

--- roles/neovim/links/config/nvim/lua/plugins/lsp.lua
+++ roles/neovim/links/config/nvim/lua/plugins/lsp.lua
@@ -1,31 +1,28 @@
--- setup an lsp with default and optional custom configuration. lsp_opts is
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(_, bufnr)
+  -- Enable completion triggered by <c-x><c-o>
+  vim.bo.omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+  -- Mappings.
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>K', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+-- setup an lsp with default and optional custom configuration. opts is
 -- a table of elements including an lsp name and the optional config.
-local function setup_lsp(lsp_opts)
-  local lspconfig = require('lspconfig')
-  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-  -- Use an on_attach function to only map the following keys
-  -- after the language server attaches to the current buffer
-  local on_attach = function(_, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-
-    -- Mappings.
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', '<F2>', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>re', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<leader>d', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<leader>K', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('i', '<c-k>', vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
-  end
-
+local function setup_lsp(module, capabilities, lsp_opts)
   local lsp_name = lsp_opts[1]
   local extra_opts = lsp_opts[2]
 
@@ -40,7 +37,7 @@ local function setup_lsp(lsp_opts)
     end
   end
 
-  lspconfig[lsp_name].setup(opts)
+  module[lsp_name].setup(opts)
 end
 
 -- config is the lazy config function
@@ -77,9 +74,6 @@ local function config()
     float = { border = 'rounded' },
     signs = false, -- disable signs in sign column
   })
-
-  local lspconfig = require('lspconfig')
-  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
   local lsps = {
     {
@@ -123,38 +117,42 @@ local function config()
       {
         on_init = function(client)
           local path = client.workspace_folders[1].name
-          if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-              Lua = {
-                runtime = {
-                  -- Tell the language server which version of Lua you're using
-                  -- (most likely LuaJIT in the case of Neovim)
-                  version = 'LuaJIT'
-                },
-                -- Make the server aware of Neovim runtime files
-                workspace = {
-                  checkThirdParty = false,
-                  library = {
-                    vim.env.VIMRUNTIME
-                    -- "${3rd}/luv/library"
-                    -- "${3rd}/busted/library",
-                  }
-                  -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-                  -- library = vim.api.nvim_get_runtime_file("", true)
-                }
-              }
-            })
-
-            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+          if vim.loop.fs_stat(path .. '/.luarc.json') or vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            return
           end
-          return true
+
+          client.config.settings.Lua = vim.tbl_deep_extend('force', client.config.settings.Lua, {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              }
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+          })
+          client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
         end,
+        settings = {
+          Lua = {}
+        },
       }
     }
   }
 
-  for _, v in pairs(lsps) do
-    setup_lsp(v)
+  local lspconfig = require('lspconfig')
+  local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+  for _, opts in pairs(lsps) do
+    setup_lsp(lspconfig, capabilities, opts)
   end
 end
 
