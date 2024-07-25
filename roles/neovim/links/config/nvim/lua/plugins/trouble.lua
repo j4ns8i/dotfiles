31a8d83e56da384d0647181ba51981e3c1056702
j4ns8i <jsmalkowski+github@gmail.com>
2024-07-25 17:15:18 -0400

    chore(neovim): update and add descriptions to trouble keys

--- roles/neovim/links/config/nvim/lua/plugins/trouble.lua
+++ roles/neovim/links/config/nvim/lua/plugins/trouble.lua
@@ -4,15 +4,46 @@ return {
     'nvim-tree/nvim-web-devicons'
   },
   keys = {
-    { '<leader>xx', function() require('trouble').close() end },
-    { '<leader>xw', function() require('trouble').open('workspace_diagnostics') end },
-    { '<leader>xc', function() require('trouble').open('lsp_incoming_calls') end },
-    { '<leader>xd', function() require('trouble').open('document_diagnostics') end },
-    { '<leader>xq', function() require('trouble').open('quickfix') end },
-    { '<leader>xl', function() require('trouble').open('loclist') end },
-    { 'gR',         function() require('trouble').open('lsp_references') end },
-    { '<leader>xi', function() require('trouble').open('lsp_implementations') end },
-    { '<leader>xs', function() require('trouble').open('lsp_document_symbols') end },
+    {
+      '<leader>xx',
+      function() require('trouble').close() end,
+      desc = "Close Trouble",
+    },
+    {
+      '<leader>xc',
+      function() require('trouble').open('lsp_incoming_calls') end,
+      desc = "LSP Incoming Calls",
+    },
+    {
+      '<leader>xd',
+      function() require('trouble').open('diagnostics') end,
+      desc = "Diagnostics",
+    },
+    {
+      '<leader>xq',
+      function() require('trouble').open('quickfix') end,
+      desc = "Quickfix",
+    },
+    {
+      '<leader>xl',
+      function() require('trouble').open('loclist') end,
+      desc = "Location List",
+    },
+    {
+      'gR',
+      function() require('trouble').open('lsp_references') end,
+      desc = "LSP References",
+    },
+    {
+      '<leader>xi',
+      function() require('trouble').open('lsp_implementations') end,
+      desc = "LSP Implementations",
+    },
+    {
+      '<leader>xs',
+      function() require('trouble').open('lsp_document_symbols') end,
+      desc = "LSP Document Symbols",
+    },
   },
   opts = {
     focus = true,
