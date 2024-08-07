5296c8ef66db740ee9b13e0cdd7369c2ca8fb1e9
j4ns8i <jsmalkowski+github@gmail.com>
2024-08-07 19:58:17 -0400

    fix(zsh): git zle bindkeys return to original buffer smoothly

--- roles/zsh/links/share/runcommands/git-fzf.zsh
+++ roles/zsh/links/share/runcommands/git-fzf.zsh
@@ -1,18 +1,22 @@
 # Step 1: Define shell functions
 _git_branches() {
+    local cursor=$CURSOR
     zle push-input
-    local branch="$(git branch -a --format '%(refname:short)' | fzf ${=FZF_GIT_OPTS})"
-    zle -U "$branch"
-    zle -U "$CUTBUFFER"
-    zle accept-line
+    local output="$(git branch -a --format '%(refname:short)' | fzf ${=FZF_GIT_OPTS})"
+    zle reset-prompt
+    zle get-line
+    zle -U "$output"
+    CURSOR=$cursor
 }
 
 _git_ls() {
+    local cursor=$CURSOR
     zle push-input
-    local commit="$(git ls --color | fzf --ansi ${=FZF_GIT_OPTS} | awk '{print $1}')"
-    zle -U "$commit"
-    zle -U "$CUTBUFFER"
-    zle accept-line
+    local output="$(git ls --color | fzf --ansi ${=FZF_GIT_OPTS} | awk '{print $1}')"
+    zle reset-prompt
+    zle get-line
+    zle -U "$output"
+    CURSOR=$cursor
 }
 
 # Step 2: Create a new ZLE command and bind it to your shell function
