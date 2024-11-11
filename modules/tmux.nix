0e7f096f16db173e79e15bd2d21aa4eb668b1e49
j4ns8i <jsmalkowski+github@gmail.com>
2024-11-11 15:21:05 -0500

    nix(tmux): use darwin.conf on darwin

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -6,9 +6,10 @@
       home.packages = with pkgs; [ tmux ];
       home.file = let
         symlink = config.lib.file.mkOutOfStoreSymlink;
+        osConf = if pkgs.stdenv.isDarwin then "darwin.conf" else "nixos.conf";
       in {
         ".tmux.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/tmux.conf";
-        ".config/tmux/os.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/nixos.conf";
+        ".config/tmux/os.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/${osConf}";
         ".config/tmux/vim-tmux-navigator.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/vim-tmux-navigator.conf";
       };
     };
