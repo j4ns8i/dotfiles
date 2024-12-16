75aab2eb3c3c6e87b4cc74ca61e077eb913ec57a
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-15 19:45:17 -0500

    tmux: use base16 theme so shell can use normal theme

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -8,9 +8,13 @@
         symlink = config.lib.file.mkOutOfStoreSymlink;
         osConf = if pkgs.stdenv.isDarwin then "darwin.conf" else "nixos.conf";
       in {
-        ".tmux.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/tmux.conf";
+        ".config/tmux/tmux.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/tmux.conf";
         ".config/tmux/os.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/${osConf}";
         ".config/tmux/vim-tmux-navigator.conf".source = symlink "${config.j4ns8i.hmDir}/config/tmux/vim-tmux-navigator.conf";
+        ".config/tmux/themes".source = fetchGit {
+          url = "https://github.com/tinted-theming/tinted-tmux";
+          rev = "20396f130e477512632c37f72590b71f59dbef88";
+        };
       };
     };
 }
