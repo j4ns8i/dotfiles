d5622e16e2f73565bd584e697162efc470f0d276
j4ns8i <jsmalkowski+github@gmail.com>
2024-12-28 20:32:57 -0500

    ghostty: add nixos config

--- machines/proton.nix
+++ machines/proton.nix
@@ -13,6 +13,7 @@
     firefox.enable = true;
     fonts.enable = true;
     fzf.enable = true;
+    ghostty.enable = true;
     git.enable = true;
     neovim.enable = true;
     tmux.enable = true;
--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -4,7 +4,9 @@
     let
       cfg = config.j4ns8i.ghostty;
       symlink = config.lib.file.mkOutOfStoreSymlink;
+      osCfg = if pkgs.stdenv.isDarwin then "darwin.toml" else "nixos.toml";
     in lib.mkIf cfg.enable {
-      home.file.".config/ghostty".source = symlink "${config.j4ns8i.hmDir}/config/ghostty";
+      home.file.".config/ghostty/config".source = symlink "${config.j4ns8i.hmDir}/config/ghostty/config";
+      home.file.".config/ghostty/os.toml".source = symlink "${config.j4ns8i.hmDir}/config/ghostty/${osCfg}";
     };
 }
