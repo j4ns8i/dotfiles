662a10951499741cc703221436ab1736af9406a7
j4ns8i <jsmalkowski+github@gmail.com>
2025-03-03 17:34:55 -0500

    neovim: use copilot chat default window instead of floating window

--- config/neovim/lua/plugins/copilot-chat.lua
+++ config/neovim/lua/plugins/copilot-chat.lua
@@ -5,12 +5,6 @@ return {
       'zbirenbaum/copilot.lua',
       'nvim-lua/plenary.nvim',
     },
-    opts = {
-      window = {
-        layout = 'float',
-        border = 'rounded',
-      },
-    },
     keys = {
       { '<leader>cpc', vim.cmd.CopilotChat,        mode = { 'n', 'v' }, desc = 'Copilot Chat' },
       { '<leader>cpe', vim.cmd.CopilotChatExplain, mode = { 'v' }, desc = 'Copilot Explain' },
