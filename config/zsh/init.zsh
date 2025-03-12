006a3f8545eb3ebd3587aace678a031f84c3b759
j4ns8i <jsmalkowski+github@gmail.com>
2025-03-11 22:09:12 -0400

    zsh: reorg init scripts to make them easier to modify

---
+++ config/zsh/init.zsh
@@ -0,0 +1,31 @@
+local function run_in_tmux() {
+    tmux new-session
+    exit 0 # exit out after the session ends or is detached
+}
+
+local function run_normally() {
+    # Uncomment for profiling
+    # zmodload zsh/zprof
+
+    export ZSH_CONFIG_DIR=~/.config/zsh
+
+    fpath=(
+        ${ZSH_CONFIG_DIR}/pure
+        $fpath
+    )
+
+    # (N) sets NULL_GLOB to return nothing when no matches are found.
+    # See `man zshexpn` > Glob Qualifiers.
+    for rc in ${ZSH_CONFIG_DIR}/local/*.zsh(N); do source $rc; done
+    for rc in ${ZSH_CONFIG_DIR}/share/*.zsh(N); do source $rc; done
+
+    # Uncomment for profiling
+    # zprof
+}
+
+# if TMUX is set or if you don't have tmux, load normally
+if [[ -n "$TMUX" ]] || [[ -f ~/.disable_tmux ]] || ! command -v tmux &>/dev/null; then
+    run_normally
+else
+    run_in_tmux
+fi
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,46 +1,16 @@
 { config, lib, pkgs, ... }: {
   options.j4ns8i.zsh.enable = lib.mkEnableOption "zsh";
   config =
-    let cfg = config.j4ns8i.zsh; in
-    lib.mkIf cfg.enable {
-      programs.zsh.enable = true;
-      programs.zsh.initExtra = ''
+    let 
+      zshrcExtra = ''
         ### BEGIN init from dotfiles/modules/zsh.nix ###
-
-        local function run_in_tmux() {
-            tmux new-session
-            exit 0 # exit out after the session ends or is detached
-        }
-
-        local function run_normally() {
-            # Uncomment for profiling
-            # zmodload zsh/zprof
-
-            export ZSH_CONFIG_DIR=~/.config/zsh
-
-            fpath=(
-                ''${ZSH_CONFIG_DIR}/pure
-                $fpath
-            )
-
-            # (N) sets NULL_GLOB to return nothing when no matches are found.
-            # See `man zshexpn` > Glob Qualifiers.
-            for rc in ''${ZSH_CONFIG_DIR}/*.zsh(N); do source $rc; done
-            for rc in ''${ZSH_CONFIG_DIR}/share/*.zsh(N); do source $rc; done
-
-            # Uncomment for profiling
-            # zprof
-        }
-
-        # if TMUX is set or if you don't have tmux, load normally
-        if [[ -n "$TMUX" ]] || [[ -f ~/.disable_tmux ]] || ! command -v tmux &>/dev/null; then
-            run_normally
-        else
-            run_in_tmux
-        fi
-
+        export ZSH_CONFIG_DIR=~/.config/zsh
+        source ''${ZSH_CONFIG_DIR}/init.zsh
         ### END init from dotfiles/modules/zsh.nix ###
       '';
+    in lib.mkIf config.j4ns8i.zsh.enable {
+      programs.zsh.enable = true;
+      programs.zsh.initExtra = zshrcExtra;
       home.packages = with pkgs; [ zsh ];
       home.file = let
         symlink = config.lib.file.mkOutOfStoreSymlink;
@@ -51,7 +21,8 @@
           rev = "a02209d36c8509c0e62f44324127632999c9c0cf";
         };
         ".config/zsh/share".source = symlink "${config.j4ns8i.hmDir}/config/zsh/share";
-        ".config/zsh/os.zsh".source = symlink "${config.j4ns8i.hmDir}/config/zsh/${osZsh}";
+        ".config/zsh/local/os.zsh".source = symlink "${config.j4ns8i.hmDir}/config/zsh/${osZsh}";
+        ".config/zsh/init.zsh".source = symlink "${config.j4ns8i.hmDir}/config/zsh/init.zsh";
       };
     };
 }
