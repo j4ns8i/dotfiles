d614b7b240aa90ca73a1b8f59b82705b7eb9a23a
j4ns8i <jsmalkowski+github@gmail.com>
2025-04-26 09:43:59 -0400

    ghostty: add third-party catppuccin themes
    The cursor colors in upstream ghostty are ultra low contrast,
    making it hard to see what's under the cursor.

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -8,5 +8,10 @@
     in lib.mkIf cfg.enable {
       home.file.".config/ghostty/config".source = symlink "${config.j4ns8i.hmDir}/config/ghostty/config";
       home.file.".config/ghostty/os.toml".source = symlink "${config.j4ns8i.hmDir}/config/ghostty/${osCfg}";
+      home.file.".config/ghostty/themes/catppuccin".source = fetchGit {
+        url = "https://github.com/catppuccin/ghostty";
+        rev = "9e38fc2b4e76d4ed5ff9edc5ac9e4081c7ce6ba6";
+      };
+      home.file.".config/ghostty/themes/catppuccin-mocha".source = symlink "${config.home.homeDirectory}/.config/ghostty/themes/catppuccin/themes/catppuccin-mocha.conf";
     };
 }
