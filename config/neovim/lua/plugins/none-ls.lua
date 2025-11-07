c44a03a66d713f28453a92c1644cd4f2dab302bc
j4ns8i <jsmalkowski+github@gmail.com>
2025-11-07 12:56:54 -0500

    neovim: remove black formatting for python

--- config/neovim/lua/plugins/none-ls.lua
+++ config/neovim/lua/plugins/none-ls.lua
@@ -5,13 +5,11 @@ return {
       local null_ls = require("null-ls")
       return {
         sources = {
-          null_ls.builtins.formatting.black,
           null_ls.builtins.diagnostics.ansiblelint,
         },
       }
     end,
     ft = {
-      'python',
       'yaml',
     },
   },
