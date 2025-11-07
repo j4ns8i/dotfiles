d7ee6806293bf77c41da225c7c984af0caf36838
j4ns8i <jsmalkowski+github@gmail.com>
2025-11-07 11:07:58 -0500

    lazygit: add lg alias

--- modules/lazygit.nix
+++ modules/lazygit.nix
@@ -9,8 +9,12 @@
   config =
     let
       cfg = config.j4ns8i.lazygit;
+      symlink = path: config.lib.file.mkOutOfStoreSymlink "${config.j4ns8i.hmDir}/${path}";
     in
     lib.mkIf cfg.enable {
       home.packages = with pkgs; [ lazygit ];
+      home.file = {
+        ".config/zsh/plugins/lazygit.zsh".source = symlink "config/lazygit/lazygit.zsh";
+      };
     };
 }
